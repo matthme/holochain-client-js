ed69edba80f031343775f341e5c3495e3fa80c79
Matthias <tuttiacc@hotmail.com>
2022-12-30 17:27:45 +0100

    built lib folder

--- lib/environments/launcher.d.ts
+++ lib/environments/launcher.d.ts
@@ -1,9 +1,9 @@
 import { InstalledAppId } from "../types.js";
 import { CallZomeRequest, CallZomeRequestSigned } from "../api/index.js";
 export interface LauncherEnvironment {
-    APP_INTERFACE_PORT: number;
-    ADMIN_INTERFACE_PORT: number;
-    INSTALLED_APP_ID: InstalledAppId;
+    APP_INTERFACE_PORT?: number;
+    ADMIN_INTERFACE_PORT?: number;
+    INSTALLED_APP_ID?: InstalledAppId;
 }
 declare const __HC_LAUNCHER_ENV__ = "__HC_LAUNCHER_ENV__";
 export declare const isLauncher: boolean;
